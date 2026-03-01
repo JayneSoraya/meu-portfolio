e616d11154f65bb7e449535dd72e06e7586b310b
Jayne Soraya <jayne.soraya@hotmail.com>
2026-03-01 19:00:20 -0300

    Arrumando prod

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -16,9 +16,6 @@ const ContactForm: React.FC = () => {
   const [showSuccess, setShowSuccess] = useState(false);
   const [error, setError] = useState('');
 
-  const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-  const API = `${BACKEND_URL}/api`;
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setFormData({
       ...formData,
@@ -32,7 +29,8 @@ const ContactForm: React.FC = () => {
     setError('');
 
     try {
-      await axios.post(`${API}/contact`, formData);
+      await axios.post('/api/contact', formData);
+      
       setShowSuccess(true);
       setFormData({ name: '', email: '', message: '' });
       
